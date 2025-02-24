750ac818e0cf0130442aea9370d85b4320f8789f
Joris327 <joris.van.bellen@gmail.com>
2025-02-24 17:11:04 +0100

    started wamup excersise

---
+++ 1/warm-up-assignment_student/Assets/Scripts/Dungeon/RectRoom.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class RectRoom
+{
+    public int x = 0;
+    public int y = 0;
+    public int width = 0;
+    public int height = 0;
+    
+    public RectRoom(int pX, int pY, int pWidth, int pHeight)
+    {
+        x = pX;
+        y = pY;
+        width = pWidth;
+        height = pHeight;
+    }
+    
+    public void SetPos(int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+    }
+    
+    public void SetSize(int newWidth, int newHeight)
+    {
+        width = newWidth;
+        height = newHeight;
+    }
+    
+    public Vector2 GetPos() => new(x, y);
+    
+    public Vector2 GetSize() => new(width, height);
+}
